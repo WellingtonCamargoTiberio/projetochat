1091939debb10784fda82808ff51ede64aee87ea
wellington tiberio <pams2ton@outlook.com>
2022-06-04 13:12:03 -0300

    initial commit

---
+++ WebChat/WebChat/Models/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebChat.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<RespostaChat> RespostaChat { get; set; }
+
+
+    }
+}
